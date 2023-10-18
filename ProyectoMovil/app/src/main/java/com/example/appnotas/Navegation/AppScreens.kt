a368424a9cb82c1d1b5c2bc49193b41cbda3c13f
Leopoldo Leonel Espino Gonzalez <Ls19120002@tecnm.surguanajuato.mx>
2023-10-17 21:06:17 -0600

    ddddddddddddddddd

--- ProyectoMovil/app/src/main/java/com/example/appnotas/Navegation/AppScreens.kt
+++
@@ -1,6 +0,0 @@
-package com.example.appnotas.Navegation
-
-sealed class AppScreens(val route: String){
-    object FirstScreen: AppScreens("first_Screen")
-    object SecondScreen: AppScreens("second_Screen")
-}
