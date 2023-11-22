734af9327d36c256d6e8e42743821bb1b41dcc60
Leopoldo Leonel Espino Gonzalez <Ls19120002@tecnm.surguanajuato.mx>
2023-11-21 21:30:10 -0600

    dsdgsdg

--- basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
+++ basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
@@ -47,12 +47,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun ReplyAppCompactPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppMediumPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Medium,
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true, widthDp = 1000)
 @Composable
 fun ReplyAppExpandedPreview() {
     ReplyTheme {
         Surface {
-            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Expanded,
+            )
         }
     }
 }
